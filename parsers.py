2a8bad4d4a24ef7dd4cd88d5d334406f573e4b5f
artem <44943333+artVG@users.noreply.github.com>
2020-10-21 21:57:56 +0300

    re fixes

--- parsers.py
+++ parsers.py
@@ -1,5 +1,6 @@
 from striprtf import strip_rtf
 import re
+from copy import deepcopy
 from typing import List, Dict
 
 
@@ -8,7 +9,7 @@ def parse_tn(file: str) -> List[Dict]:
     result: list = []
     for line in file:
         line = line.replace('\xa0', '')
-        if re.findall(r"\|[0-9]+\|[0-9]{2}\.[0-9]{2}\.[0-9]+\|[0-9]+", line):
+        if re.findall(r'[0-9]*\|[0-9]{2}\.[0-9]{2}\.[0-9]+\|[0-9]*', line):
             line_list: list = line.split('|')
             doc: dict = {
                 'series': line_list[0],
@@ -18,16 +19,17 @@ def parse_tn(file: str) -> List[Dict]:
                 'contract': line_list[4]
             }
             result.append(doc)
+    print('parse_tn', len(result))
     return result
 
 
 def parse_transaction(file: str) -> List[Dict]:
     file: List[str] = strip_rtf(file).split('\n')
     result: list = []
+    i = 0
     for line in file:
         line = line.replace('\xa0', '')
-        print(line)
-        if re.findall(r"[0-9]+\|[0-9]{2}\.[0-9]{2}\.[0-9]+\|*\|[0-9]+", line):
+        if re.findall(r'[0-9]*\|[0-9]{2}\.[0-9]{2}\.[0-9]+\|', line):
             line_list: list = line.split('|')
             doc: dict = {
                 'number': line_list[0],
@@ -36,11 +38,13 @@ def parse_transaction(file: str) -> List[Dict]:
                 'price': line_list[4],
             }
             result.append(doc)
+    print('parse_transaction', len(result))
     return result
 
 
 def add_tn_to_transaction(transactions: List[Dict], tns: List[Dict]) -> List[Dict]:
-    for tr in transactions:
+    result: List[Dict] = deepcopy(transactions)
+    for tr in result:
         for tn in tns:
             if tr['number'] == tn['number']:
                 tr['series'] = tn['series']
@@ -48,3 +52,5 @@ def add_tn_to_transaction(transactions: List[Dict], tns: List[Dict]) -> List[Dic
                 tr['sum'] = tn['sum']
                 tr['contract'] = tn['contract']
                 break
+    print('add_tn_to_transaction', len(result))
+    return result
