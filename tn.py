90cd27895b5dc7e9fba0c906e5a9415971ce2602
artem <44943333+artVG@users.noreply.github.com>
2020-10-22 20:23:48 +0300

    new tn class

--- tn.py
+++ tn.py
@@ -1,23 +1,19 @@
-from settings import TN_NUMBER_LEN
-from datetime import date
+from document import Document
+from transaction import Transaction
+from typing import List
 
 
 class TN:
-    def __init__(self, number :int, date :date) -> None:
-        self.number = number
-        self.date = date
-        
-    def __repr__(self):
-        str_number = str(self.number)
-        number_len = len(str_number)
-        if number_len < TN_NUMBER_LEN:
-            str_number = ('0'* (TN_NUMBER_LEN - number_len)) + str_number
-        return str_number + ' \\ ' + str(self.date)
+    def __init__(self,
+                 document: Document = Document(),
+                 transactions_sum: str = '',
+                 contract: str = '',
+                 transactions: List[Transaction] = []
+                 ) -> None:
+        self.document = document
+        self.transactions_sum = transactions_sum
+        self.contract = contract
+        self.transactions = transactions
 
-    def __eq__(self, other):
-        return (self.number == other.number and
-                self.date == other.date)
-
-    def __lt__(self, other):
-        return (self.date < other.date or
-            (self.date == other.date and self.number < other.number))
+    def __repr__(self) -> str:
+        return f'{self.document} {self.contract} {self.transactions_sum}{self.transactions}'
