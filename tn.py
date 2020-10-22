5ebf598be27571d2a57c36d20a86abc1f1fbdc44
artem <44943333+artVG@users.noreply.github.com>
2020-10-22 21:35:10 +0300

    aded method that creates list of TNs from dict

--- tn.py
+++ tn.py
@@ -17,3 +17,38 @@ class TN:
 
     def __repr__(self) -> str:
         return f'{self.document} {self.contract} {self.transactions_sum}{self.transactions}'
+
+    def check_eq_except_transactions(self, other) -> bool:
+        pass
+
+    def add_transactions_from(self, other) -> None:
+        pass
+
+
+def from_dict(tn_dict: dict) -> List[TN]:
+    result: List[TN] = []
+    for tn_data in tn_dict:
+        new_tn: TN = TN(
+            document=Document(
+                number=tn_data['number'],
+                series=tn_data['series'],
+                date=tn_data['date']
+            ),
+            transactions_sum=tn_data['sum'],
+            contract=tn_data['contract'],
+            transactions=[
+                Transaction(
+                    product=tn_data['product'],
+                    amount=tn_data['amount'],
+                    price=tn_data['price']
+                )
+            ]
+        )
+        found: bool = False
+        for tn in result:
+            if new_tn.check_eq_except_transactions(tn):
+                tn.add_transactions_from(new_tn)
+                found = True
+        if not found:
+            result.append(new_tn)
+    return result
