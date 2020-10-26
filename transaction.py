33a3c2c3f6fc6eed1458b391dbf4dd8c83b00413
artVG <lifeavg@gmail.com>
2020-10-26 19:59:31 +0300

    new method for equality checking

--- transaction.py
+++ transaction.py
@@ -8,3 +8,9 @@ class Transaction:
     def __repr__(self) -> str:
         return f'\n\t-> {self.product} -{self.amount}- {self.price}'
 
+    def check_eq_except_amount(self, other) -> bool:
+        return (
+            self.product == other.product and
+            self.price == other.price
+        )
+
