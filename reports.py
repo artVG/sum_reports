e18748383765a01f778424d99e57e959ca9f1efc
artVG <lifeavg@gmail.com>
2020-10-26 21:01:53 +0300

    adopted for new tn class

--- reports.py
+++ reports.py
@@ -1,73 +1,63 @@
-def sort_transactions_contract(transactions :list) -> list:
+from typing import List
+from tn import TN
+from transaction import Transaction
+
+
+def sort_tn_by_contract(tns: List[TN]) -> List[List[TN]]:
     contract_sorted = []
-    for transaction in transactions:
+    for tn in tns:
         found = False
         for sublist in contract_sorted:
             if found:
-                pass
+                break
             elif sublist:
-                if sublist[0].contract == transaction.contract:
-                    sublist.append(transaction)
+                if sublist[0].contract == tn.contract:
+                    sublist.append(tn)
                     found = True
         if not found:
-            contract_sorted.append([transaction, ])
+            contract_sorted.append([tn, ])
     return contract_sorted
 
-def sum_transactions_name(transactions :list) -> list:
+
+def sum_by_transaction(tns: List[TN]) -> List[Transaction]:
     sum_transactions = []
-    for transaction in transactions:
-        found = False
-        for sumed_transaction in sum_transactions:
-            if found:
-                pass
-            elif sumed_transaction:
-                if sumed_transaction.name == transaction.name:
-                    sumed_transaction.amount+=transaction.amount
-                    found = True
-        if not found:
-            sum_transactions.append(transaction)
+    for tn in tns:
+        for transaction in tn.transactions:
+            found = False
+            for summed_transaction in sum_transactions:
+                if found:
+                    break
+                # elif summed_transaction:
+                else:
+                    if summed_transaction.check_eq_except_amount(transaction):
+                        summed_transaction.amount += transaction.amount
+                        found = True
+            if not found:
+                sum_transactions.append(transaction)
     return sum_transactions
 
-def f(t):
-    for i in t:
-        if i.amount > 15:
-            print(i)
 
-def report_sorted_contract_sum_transactions_name(transactions :list) -> list:#----------------------
-    f(transactions)
-    contract_sorted = sort_transactions_contract(transactions)
+def report_sorted_by_contract_sum_by_transaction(tns: List[TN]) -> List[List[Transaction]]:
+    contract_sorted = sort_tn_by_contract(tns)
     report = []
     for contract in contract_sorted:
-        sumed_transactions_name = sum_transactions_name(contract)
-        report.append(sumed_transactions_name)
+        summed_transactions_name = sum_by_transaction(contract)
+        report.append(summed_transactions_name)
     return report
 
-def report_sorted_contract(transactions :list) -> list:
-    f(transactions)
-    contract_sorted = sort_transactions_contract(transactions)
-    report = []
-    for contract in contract_sorted:
-        report.append(sorted(contract))
+
+def report_sorted_by_contract(tn: List[TN]) -> List[List[TN]]:
+    report = sort_tn_by_contract(tn)
+    for contract in report:
+        contract.sort(key=lambda x: x.date)
     return report
 
-def report_sum_transactions_name(transactions :list) -> list:#---------------------------
-    f(transactions)
-    report = sum_transactions_name(transactions)
-    return report
 
-def report_list_tn(transactions :list) -> list:
-    f(transactions)
-    sorted_transactions = sorted(transactions)
-    report = []
-    for transaction in sorted_transactions:
-        found = False
-        for sublist in report:
-            if found:
-                pass
-            elif sublist:
-                if sublist[0].tn == transaction.tn:
-                    sublist.append(transaction)
-                    found = True
-        if not found:
-            report.append([transaction, ])
-    return report
+def report_sum_transactions_name(tns: List[TN]) -> List[Transaction]:  # ---------------------------
+    return sum_by_transaction(tns)
+
+
+def report_list_tn(tns: List[TN]) -> List[TN]:
+    return tns
+
+
